689df2540b701ffbba480b516baf7cc40fc20197
Ankit Singh <Griffintaur@users.noreply.github.com>
2017-08-08 11:14:15 +0530

    Revert "Fixed Input error"

--- Main.py
+++ Main.py
@@ -21,49 +21,32 @@ def App():
         for i in xrange(len(newsSources)):
             print "["+str(i)+"]" +"\t" +newsSources[i]
         print "Please enter the index of the news source or press 99 to quit"
-
-        sourceNumberOkay = False
-        while sourceNumberOkay == False:
-            try:
-                newsSourceNumber=raw_input("News Source Number >>>>")
-                newsSourceNumber=int(newsSourceNumber)
-
-                if (newsSourceNumber >=len(newsSources)):
-                    print "please select an index less than "+ str(len(newsSources))
-                else:
-                    sourceNumberOkay = True
-            except ValueError:
-                print("That is not a valid News source number")    
-
+        try:
+            newsSourceNumber=raw_input("News Source Number >>>>")
+        except ValueError:  
+            print("That is not a valid News source number")
+        newsSourceNumber=int(newsSourceNumber)
         if newsSourceNumber==99:
             sys.exit()
-          
+        if (newsSourceNumber >=len(newsSources)):
+            print "please select the index no less than "+ str(len(newsSources))  
         obj=NewsPulling(newsSources[newsSourceNumber])
         Articles=obj.BeautifyArticles();   
         while True:
             print """Do you want to read any story further?.if yes,Please Select the Number corresponding to the article """
             print "Press 66 to go back to the main menu"
             print "Press 99 to quit"
-            
-            articleNumberOkay = False
-            while articleNumberOkay == False:
-                try:
-                    articleNumber=raw_input("Article No >>>>")
-                    articleNumber=int(articleNumber)
-
-                    if (articleNumber >= len(Articles)):
-                        print "please select an index less than "+ str(len(Articles))
-                    else:
-                        articleNumberOkay = True
-                except ValueError:
-                    print ("That's not a valid article number")
-
-            
+            try:
+                articleNumber=raw_input("Article No >>>>")
+            except ValueError:
+                print ("That's not a valid article number")
+            articleNumber=int(articleNumber)
             if articleNumber==99 :
                 sys.exit()
             if articleNumber==66 :
                 break
-            
+            if (articleNumber >= len(Articles)):
+                print "please select the index no less than "+ str(len(Articles))
             #print Articles[articleNumber][2]
             extr=ExtractMainContent(newsSources[newsSourceNumber],Articles[articleNumber][2])
             extr.Beautify()
